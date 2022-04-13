bcdba304282e5fafe154c232bd175ba63946747c
divya8210 <divya.aquarius@gmail.com>
2022-04-13 16:05:24 -0700

    eslint unfixed problem

---
+++ .eslintrcold.js
@@ -0,0 +1,11 @@
+// // .eslintrc.js
+// module.exports = {
+// 	"rules": {
+// 	  "no-mixed-spaces-and-tabs": 0, // disable rule,
+// 	  "vue/script-indent": ["error", 4, { "baseIndent": 1 }]
+// 	},
+// 	parserOptions: {
+// 		parser: 'babel-eslint',
+// 		ecmaVersion: 6
+// 	},
+//   }
